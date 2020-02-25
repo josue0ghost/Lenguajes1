051a1fe4f3d284adb9815e6b2efe51a3f14f9801
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-02-25 13:34:25 -0600

    Mejoras
    Quitar espacios y tabulaciones donde es necesario en los sets y tokens
    
    analizad tokens iniciado

--- Proyecto_lenguajes/Proyecto_lenguajes/FileReader.cs
+++ Proyecto_lenguajes/Proyecto_lenguajes/FileReader.cs
@@ -10,6 +10,7 @@ namespace Proyecto_lenguajes
 	class FileReader
 	{
 		public Dictionary<string, string> Sets { get; set; }
+		public Dictionary<string, List<string>> Tokens { get; set; }
 		public string Error { get; set; }
 		private bool ContainsSets { get; set; }
 
@@ -36,7 +37,10 @@ namespace Proyecto_lenguajes
 			{
 				Error = SETS(Sets);
 			}
-			
+			if (Error == "")
+			{
+				Error = TOKENS(Tokens);
+			}
 
 			this.Error = Error;
 		}
@@ -50,6 +54,8 @@ namespace Proyecto_lenguajes
 		/// <returns></returns>
 		private bool ContainsAllSections(string FileContent, ref string Error)
 		{
+			FileContent = FileContent.Replace(" ", "");
+			FileContent = FileContent.Replace('\t'.ToString(), "");
 			this.ContainsSets = FindSection("SETS", FileContent);
 			if (!FindSection("TOKENS", FileContent, ref Error)) return false;
 			if (!FindSection("ACTIONS", FileContent, ref Error)) return false;
@@ -110,7 +116,7 @@ namespace Proyecto_lenguajes
 
 			for (int i = 0; i < SetsArray.Length; i++)
 			{
-				if (SetsArray[i] != "")
+				if (SetsArray[i].Trim().Replace('\t'.ToString(), "") != "")
 				{
 					if (!AnalizeSet(SetsArray[i]))
 					{
@@ -124,11 +130,18 @@ namespace Proyecto_lenguajes
 
 		internal bool AnalizeSet(string Set)
 		{
-			bool ValidSet = false;
+			bool ValidSet = true;
 			Set.Trim();
 
-			string id = Set.Substring(0, Set.IndexOf(Utilities.EqualsSign));
-			char[] expression = Set.Substring(Set.IndexOf(Utilities.EqualsSign) + 1).Trim().ToCharArray();			
+			if (!Set.Contains(Utilities.EqualsSign.ToString()))
+			{
+				this.Error = "Formato de set incorrecto. Se esperaba asignación.";
+				return !ValidSet;
+			}
+
+			string id = Set.Substring(0, Set.IndexOf(Utilities.EqualsSign)).Replace(" ", "").Replace('\t'.ToString(), "");
+			string value = Set.Substring(Set.IndexOf(Utilities.EqualsSign) + 1).Trim().Replace('\t'.ToString(), "");
+			char[] expression = value.ToCharArray();
 
 			for (int i = 0; i < expression.Length; i++)
 			{
@@ -140,18 +153,17 @@ namespace Proyecto_lenguajes
 						if (expression[i + 2] != Utilities.CharLimiter)
 						{
 							this.Error = "No se puede convertir explicitamente <string> en <char>";
-							return ValidSet;
+							return !ValidSet;
 						}
 					}
 					catch (Exception)
 					{
 						this.Error = "Se esperaba caracter de cierre: ".Concat(Utilities.CharLimiter.ToString()).ToString();
-						return ValidSet;
+						return !ValidSet;
 					}
 					
 					i += 2;
 				}
-
 				// para funciones CHR
 				else if (expression[i] == 'C')
 				{
@@ -160,13 +172,13 @@ namespace Proyecto_lenguajes
 						if (expression[i+1] != 'H' || expression[i+2] != 'R')
 						{
 							this.Error = "No se reconoce la funcion escrita";
-							return ValidSet;
+							return !ValidSet;
 						}
 
 						if (expression[i + 3] != Utilities.OpenAgrupation)
 						{
 							this.Error = "Se esperaba caracter de apertura: ".Concat(Utilities.OpenAgrupation.ToString()).ToString();
-							return ValidSet;
+							return !ValidSet;
 						}
 
 						i += 4;
@@ -177,7 +189,7 @@ namespace Proyecto_lenguajes
 							if (!int.TryParse(expression[i].ToString(), out int x))
 							{
 								this.Error = "Se esperaba numero entero";
-								return ValidSet;
+								return !ValidSet;
 							}
 							i++;
 						}
@@ -185,10 +197,9 @@ namespace Proyecto_lenguajes
 					catch (Exception)
 					{
 						this.Error = "Formato de función CHR(<int>) incorrecto";
-						return ValidSet;
+						return !ValidSet;
 					}
 				}
-
 				// Maneja los rangos ("..")
 				else if (expression[i] == Utilities.Range)
 				{
@@ -197,47 +208,146 @@ namespace Proyecto_lenguajes
 						if (expression[i + 1] != Utilities.Range)
 						{
 							this.Error = "Se esperaba: ".Concat(Utilities.Range.ToString()).ToString();
-							return ValidSet;
+							return !ValidSet;
 						}	
 					}
 					catch (Exception)
 					{
 						this.Error = "Formato de set incorrecto";
-						return ValidSet;
+						return !ValidSet;
 					}					
 					i += 1;
 				}
+				// Maneja las concatenaciones ('+')
 				else if (expression[i] == Utilities.ConcatSign)
 				{
 					try
 					{
 						if (!(expression[i + 1] == '\'' || expression[i + 1] == 'C'))
 						{
-							this.Error = "Concatenación incorrecta";
-							return ValidSet;
+							this.Error = "Concatenación incorrecta. Se esperaba expresion de tipo '<char>' o CHR(<int>)";
+							return !ValidSet;
 						}
 					}
 					catch (Exception)
 					{
-						this.Error = "Se esperaba expresion de tipo '<char>' o CHR(<int>)";
-						return ValidSet;
+						this.Error = "Concatenación incorrecta. Se esperaba expresion de tipo '<char>' o CHR(<int>)";
+						return !ValidSet;
 					}										
 				}
 				else // precede un caracter no esperado
 				{
 					this.Error = "Caracter no esperado";
-					return ValidSet;
+					return !ValidSet;
 				}
 			}
+
+			if (!Sets.ContainsKey(id))
+			{
+				Sets.Add(id, value);
+			}
+			else
+			{
+				this.Error = "El set ya fue definido anteriormente";
+				return !ValidSet;
+			}
+
+			return ValidSet;
+		}
+
+		internal string TOKENS(string Tokens)
+		{
+			string[] TokensArray = Tokens.Split('\n');
+
+			for (int i = 0; i < TokensArray.Length; i++)
+			{
+				if (TokensArray[i] != "")
+				{
+					if (!AnalizeToken(TokensArray[i]))
+					{
+						return this.Error;
+					}
+				}
+			}
+			return "";
+		}
+
+		internal bool AnalizeToken(string Token)
+		{
+			bool ValidToken = true;
+
+			List<string> items = new List<string>();
+
+			if (!Token.Contains(Utilities.EqualsSign))
+			{
+				this.Error = "Formato de token incorrecto";
+				return !ValidToken;
+			}
 			
-			// TO DO
-			// hay error si lo que está entre comillas tiene tamño mayor a 1 
-			// Si hay un punto debe estar después de una comilla o paréntesis
-			// Si hay un punto debe estar seguido de otro punto (si este no está entre comillas)
-			// Si hay un signo + debe estar seguido de una comilla o C
 
+			string id = Token.Substring(0, Token.IndexOf(Utilities.EqualsSign)).Replace('\t'.ToString(), "").Replace(" ", "");			
 
-			return true;
+			// si existe la cadena
+			if (id.IndexOf("TOKEN", StringComparison.OrdinalIgnoreCase) >= 0)
+			{
+				string num = "";
+				char[] idChar = id.ToCharArray();
+				for (int i = 5; i < idChar.Length; i++)
+				{
+					if (int.TryParse(idChar[i].ToString(), out _))
+					{
+						num.Concat(idChar[i].ToString());
+					}
+					else
+					{
+						this.Error = "Se esperaba valor de tipo <int>";
+						return !ValidToken;
+					}
+				}
+			}
+			else
+			{
+				this.Error = "Falta la palabra 'TOKEN' en la definicion del identificador token";
+				return !ValidToken;
+			}
+
+			char[] RE = Token.Substring(Token.IndexOf(Utilities.EqualsSign) + 1).Trim().ToCharArray();
+
+			for (int i = 0; i < RE.Length; i++)
+			{				
+				if (RE[i] == Utilities.CharLimiter)
+				{
+					try
+					{
+						if (RE[i + 2] != Utilities.CharLimiter)
+						{
+							this.Error = "No se puede convertir explicitamente <string> en <char>. Falta caracter de cierre: ".Concat(Utilities.CharLimiter.ToString()).ToString();
+							return ValidToken;
+						}
+						else
+						{
+							items.Add(RE[i + 1].ToString());
+						}
+					}
+					catch (Exception)
+					{
+						this.Error = "Se esperaba caracter de cierre: ".Concat(Utilities.CharLimiter.ToString()).ToString();
+						return ValidToken;
+					}
+
+					i += 2;
+				}
+				else
+				{
+					// identificar id de expresiones regulares
+					while (true)
+					{
+
+					}
+				}
+			}
+
+			return ValidToken;
 		}
 	}
 }
