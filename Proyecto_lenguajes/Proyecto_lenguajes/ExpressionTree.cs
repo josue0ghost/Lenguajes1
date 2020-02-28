be3053474e2e651f44712583a87c412ef115312d
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-02-28 16:43:20 -0600

    Update ExpressionTree.cs

--- Proyecto_lenguajes/Proyecto_lenguajes/ExpressionTree.cs
+++ Proyecto_lenguajes/Proyecto_lenguajes/ExpressionTree.cs
@@ -8,30 +8,101 @@ namespace Proyecto_lenguajes
 {
 	class Node
 	{
+		public Node Left { get; set; }
+		public Node Right { get; set; }
+		string item { get; set; }
+		public bool Leaf => (this.Left == null && this.Right == null);
+		public bool Full => (this.Left != null && this.Right != null);
+
+		public Node() { }
+
+		public Node(string value) : this(value, null, null) { }
+		
+		public Node(string value, Node Left, Node Right)
+		{
+			this.item = value;
+			this.Left = Left;
+			this.Right = Right;
+		}
 
 	}
 	class ExpressionTree
 	{
-		public Node Father;
-		public Node Left;
-		public Node Right;
+		public Node Root { get; set; }
 		public string Error;
 		private Stack<string> items;
 		private Stack<ExpressionTree> trees;
-		
 
-		public void CreateTree(char[] RE)
+		public ExpressionTree()
+		{
+
+		}
+
+		/// <summary>
+		/// Algoritmo creado por Moises Alonso
+		/// Algoritmo de creacion de arbol de expresion a traves de expresion regular
+		/// </summary>
+		/// <param name="RE"></param>
+		public void CreateTree(char[] RE, string id)
 		{
 			string set = "";
 			for (int i = 0; i < RE.Length; i++)
 			{
 				if (RE[i] == '(')
 				{
-
-				}
-				else if (RE[i] == '|' || RE[i] == '?' || RE[i] == '*' || RE[i] == '+')
-				{
 					items.Push(RE[i].ToString());
+				}
+				else if (RE[i] == ')')
+				{
+					while (items.Count > 0 && items.Peek() != "(")
+					{
+						if (items.Count == 0)
+						{
+							this.Error = "Faltan operandos en la expresion regular asignada a ".Concat(id)
+								.Concat(". No se puede cerrar una agrupacion sin antes abrirla").ToString();
+							return;
+						}
+						if (trees.Count < 2)
+						{
+							this.Error = "Faltan operandos en la expresion regular asignada a ".Concat(id).ToString();
+							return;
+						}
+
+						ExpressionTree temp = new ExpressionTree();
+						Node node = new Node(items.Pop());
+						temp.Root = node;
+
+						ExpressionTree Right = trees.Pop();
+						temp.Root.Right = Right.Root;
+						ExpressionTree Left = trees.Pop();
+						temp.Root.Left = Left.Root;
+
+						trees.Push(temp);
+					}
+
+					items.Pop();
+				}
+				else if (RE[i] == '?' || RE[i] == '*' || RE[i] == '+')
+				{
+					ExpressionTree temp = new ExpressionTree();
+					Node node = new Node(RE[i].ToString());
+					temp.Root = node;
+
+					if (trees.Count == 0)
+					{
+						this.Error = "Faltan operandos";
+						return;
+					}
+
+					ExpressionTree Left = new ExpressionTree();
+					Left = trees.Pop();
+					temp.Root.Left = Left.Root;
+
+					trees.Push(temp);
+				}
+				else if (RE[i] == '|')
+				{
+
 				}
 				else if (RE[i] == Utilities.CharLimiter)
 				{
@@ -68,5 +139,7 @@ namespace Proyecto_lenguajes
 				}
 			}
 		}
+
+
 	}
 }
